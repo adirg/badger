9bf0f3a07c1d66c903c679def15138e95af8faf9
Adir Gabai <adirgabai@gmail.com>
2017-06-19 12:00:55 +0300

    benchmark: Added latency stats.

--- benchmark/benchmark.go
+++ benchmark/benchmark.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"math/rand"
 	"os"
+	"sort"
 	"sync"
 	"time"
 
@@ -43,6 +45,7 @@ func (bench *Benchmark) Destroy() {
 func (bench *Benchmark) Load() {
 	fmt.Println("Loading...")
 
+	latency := make([]int64, bench.opt.Records)
 	var wg sync.WaitGroup
 	start := time.Now()
 	keysPerThread := bench.opt.Records / bench.opt.LoadThreads
@@ -51,43 +54,46 @@ func (bench *Benchmark) Load() {
 		go func(kv *badger.KV, startKey int) {
 			value := make([]byte, bench.opt.ValueSize)
 			rand.Read(value)
-			fmt.Printf("Start Key: %d\n", startKey)
 			for j := startKey; j < startKey+keysPerThread; j++ {
 				key := fmt.Sprint(j)
+				start := time.Now()
 				kv.Set([]byte(key), value)
+				latency[j] = time.Since(start).Nanoseconds()
+
 			}
 			wg.Done()
 		}(bench.kv, i*keysPerThread)
 	}
 	wg.Wait()
 	elapsed := time.Since(start)
-	fmt.Printf("ops = %d\n", bench.opt.Records)
-	fmt.Printf("elapsed = %s\n", elapsed)
+
+	printStats("load", bench.opt.Records, elapsed, latency)
 }
 
 func (bench *Benchmark) Run() {
 	fmt.Println("Running...")
 
+	latency := make([]int64, bench.opt.Records)
 	var wg sync.WaitGroup
 	start := time.Now()
 	keysPerThread := bench.opt.Records / bench.opt.RunThreads
 	for i := 0; i < bench.opt.RunThreads; i++ {
 		wg.Add(1)
 		go func(kv *badger.KV, startKey, idx int) {
-			fmt.Printf("Start Key: %d\n", startKey)
 			for j := startKey; j < startKey+keysPerThread; j++ {
-				key := fmt.Sprint(j)
+				key := fmt.Sprint(startKey + rand.Intn(keysPerThread))
 				var item badger.KVItem
 				item.Hint = idx
+				start := time.Now()
 				kv.Get([]byte(key), &item)
+				latency[j] = time.Since(start).Nanoseconds()
 			}
 			wg.Done()
 		}(bench.kv, i*keysPerThread, i)
 	}
 	wg.Wait()
 	elapsed := time.Since(start)
-	fmt.Printf("ops = %d\n", bench.opt.Records)
-	fmt.Printf("elapsed = %s\n", elapsed)
+	printStats("run", bench.opt.Records, elapsed, latency)
 }
 
 func prepareKVDir(dir string) {
@@ -128,4 +134,37 @@ func max(a, b uint) uint {
 		return b
 	}
 	return a
+}
+
+func printStats(stage string, numOps int, period time.Duration, latency []int64) {
+	fmt.Printf("===== %s stats\n", stage)
+	printIOPS(numOps, period)
+	printLatencyStats(latency)
+	fmt.Printf("=====\n")
+}
+
+func printIOPS(numOps int, period time.Duration) {
+	fmt.Printf("iops = %f\n", float64(numOps)/period.Seconds())
+}
+
+func printLatencyStats(latency []int64) {
+	n := len(latency)
+	sort.Slice(latency, func(i, j int) bool { return latency[i] < latency[j] })
+
+	min := latency[0]
+	max := latency[n-1]
+
+	sum := int64(0)
+	for i := range latency {
+		sum += latency[i]
+	}
+	avg := float64(sum) / float64(n)
+
+	fmt.Println("latency stats:")
+	fmt.Println("max: ", max)
+	fmt.Println("min: ", min)
+	fmt.Println("avg: ", avg)
+
+	p99idx := int(math.Ceil(0.99 * float64(n)))
+	fmt.Println("p99 latency: ", latency[p99idx])
 }
